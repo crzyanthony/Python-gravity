722df3a681ddc5c4d69e4535a355a6ff7587413d
crzyanthony <44053390+crzyanthony@users.noreply.github.com>
2018-10-12 08:54:42 -0700

    GRAVITY AND HEIGHT!!!!!

--- gravity.py
+++ gravity.py
@@ -1,7 +1,10 @@
 # Oct-12-2018
 
+GRAVITY=9.8 
+HEIGHT_0=16
+
 def calcFallTime(t, v, h):
-    h=-16*t*t+v*t+h
+    h=-GRAVITY/2*t*t+v*t+h
     return h
 
 
@@ -9,11 +12,11 @@ print "Calculate the height you will achieve"
 
 time=0.6 
 velocity=16
-height=16
+height=HEIGHT_0
 
 final=calcFallTime(time, velocity, height)
 print "After 1.6 seconds with a starting"
-print "velocity of 16 ft/sec and a starting"
+print "velocity of ft/sec and a starting"
 print "height of 16 feet"
 print ""
 print "You will achieve "+str(final)+" feet"
